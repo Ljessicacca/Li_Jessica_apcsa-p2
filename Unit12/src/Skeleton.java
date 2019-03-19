4fd10957e4e58f964b836ae84fa2ec9998f43410
Ljessicacca <47190030+Ljessicacca@users.noreply.github.com>
2019-03-19 09:34:59 -0700

    U 12

---
+++ Unit12/src/Skeleton.java
@@ -0,0 +1,78 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*; 
+
+public class Skeleton implements Monster
+{
+	//add instance variables
+	private String name; 
+	private int size;  
+	
+	//add a constructor		
+	public Skeleton(String x, int y)
+	{
+		name = x; 
+		size = y; 
+	}
+
+	//add code to implement the Monster interface
+	public int getHowBig()
+	{
+		return size; 
+	}
+	
+	public String getName()
+	{
+		return name; 
+	}
+	
+	public boolean isBigger(Monster other)
+	{
+		if (this.getHowBig()>other.getHowBig())
+		{
+			return true; 
+		}
+		return false; 
+	}
+	
+	public boolean isSmaller(Monster other)
+	{
+		if (this.getHowBig()<other.getHowBig())
+		{
+			return true; 
+		}
+		return false;
+	}
+	
+	public boolean namesTheSame(Monster other)
+	{
+		if (this.getName().equals(other.getName()))
+		{
+			return true;
+		}
+		return false;
+		
+	}
+
+	//add a toString
+	public String toString()
+	{
+		String output = this.getName() + " " + this.getHowBig();
+		/*if (this.isBigger())
+		{
+			output += this.getName() + " is bigger. ";
+		}
+		if (this.isSmaller())
+		{
+			output += this.getName() + " is smaller. ";
+		}
+		if (this.namesTheSame())
+		{
+			output += this.getName() + " has the same name as " + skeleton.getName(); 
+		}*/
+		return output; 
+		
+	}
+}
